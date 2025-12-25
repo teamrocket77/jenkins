8a740550b14a20b272c725398a67c079130ccd9a
Vincent Cradler <vincentcradler@gmail.com>
2025-12-24 17:02:27 -0800

    updated arg parse to work with method decorators
    added new functionality for validation

--- src/main.py
+++ src/main.py
@@ -1,64 +1,133 @@
-import os
-import sys
-import jenkins
 import argparse
-from pprint import pprint
+from http import HTTPStatus
+import os
+from collections.abc import Callable
+import sys
+from pathlib import Path
+from enum import StrEnum
+import inspect
+import requests
+
+import jc
+import jenkins
 
 JENKINS_USER = os.environ["JENKINS_USERNAME"]
 JENKINS_TOKEN = os.environ["JENKINS_TOKEN"]
-parser = argparse.ArgumentParser(
-    "jenkins cli"
-)
-parser.add_argument("-hw", action="store_true", help="Hello world", dest="hello_world")
-parser.add_argument("-ls", action="store_true", help="list jobs", dest="list_jobs")
-parser.add_argument("-c", action="store_true", help="New Job", dest="new_job")
-parser.add_argument("-d", help="Download Job", dest="download_job")
+parser = argparse.ArgumentParser("jenkins cli")
 
-args = parser.parse_args()
 
-def get_connection():
+def get_connection() -> jenkins.Jenkins:
     return jenkins.Jenkins(
-        'http://localhost:8000',
+        "http://localhost:8000",
         username=JENKINS_USER,
-        password=JENKINS_TOKEN
+        password=JENKINS_TOKEN,
     )
 
-def hello_world(server: jenkins.Jenkins):
-    user = server.get_whoami()
-    version = server.get_version()
-    print('Hello %s from Jenkins %s' % (user['fullName'], version))
 
-def new_job(server: jenkins.Jenkins):
-    print(jenkins.EMPTY_CONFIG_XML)
+def cli_command() -> Callable:
+    def decorate(func: Callable) -> Callable:
+        func.is_visible = True
+        return func
 
-def list_jobs(server: jenkins.Jenkins):
-    jobs = server.get_jobs()
-    print(jobs)
+    return decorate
 
-def save_job(job_data:str, job_name: str):
-    with open('jobs/' + job_name + ".xml", 'w') as f:
-        f.write(job_data)
-    print(f"job name: {job_name} saved")
 
-def download_job(server: jenkins.Jenkins, job_name: str):
-    if (name := server.get_job_name(job_name)) is not None:
-        job_info = server.get_job_config(name)
-        print(job_info)
-        save_job(job_info, job_name)
-        return
-    raise Exception("Job not found")
+class Jenkins:
+    """Contains methods for Jenkins class."""
 
-def main():
-    if len(sys.argv) == 1:
-        args.print_help()
-    conn = get_connection()
-    if args.hello_world:
-        hello_world(conn)
-    elif args.list_jobs:
-        list_jobs(conn)
-    elif args.new_job:
-        new_job(conn)
-    elif args.download_job:
-        download_job(conn, args.download_job)
-    else:
-        parser.print_help()
+    def __init__(self):
+        self.connection = get_connection()
+
+    @staticmethod
+    def new_job(args) -> None:
+        """Not impl."""
+        print(jenkins.EMPTY_CONFIG_XML)
+
+    @cli_command()
+    def list_jobs(self) -> None:
+        jobs = self.connection.get_jobs()
+        print(jobs)
+
+    @staticmethod
+    def save_job(job_data: str, job_name: str) -> None:
+        with Path("jobs/" + job_name + ".xml").open("w") as f:
+            f.write(job_data)
+        print(f"job name: {job_name} saved")
+
+    @cli_command()
+    def download_job(self, job_name: str) -> str:
+        server = self.connection
+        if (name := server.get_job_name(job_name)) is not None:
+            job_info = server.get_job_config(name)
+            print(job_info)
+            Jenkins.save_job(job_info, job_name)
+            return job_info
+        msg = "Job not found"
+        raise Exception(msg)
+
+    def update_job(self, job_name: str) -> None:
+        Jenkins.validate_job(job_name)
+
+    @staticmethod
+    @cli_command()
+    def validate_job(fname: str):
+        data = open("jobs/" + fname).read()
+        if not data:
+            raise Exception("Data is empty")
+        form_data = {"Jenkinsfile": data}
+        jenkins_host = (
+            f"http://{os.environ['JENKINS_HOST']}/pipeline-model-converter/validate"
+        )
+        session = requests.Session()
+        response = session.post(
+            jenkins_host, auth=(JENKINS_USER, JENKINS_TOKEN), data=form_data
+        )
+        if response.status_code != HTTPStatus.OK:
+            msg = f"Request to url: {os.environ['JENKINS_HOST']} was {response.status_code}.. {response.text if response.text else response.json()}"
+            raise Exception(msg)
+        print(f"Job: {fname} successfully validated")
+
+
+def create_cli(cls):
+    parser = argparse.ArgumentParser(description="Jenkins CLI")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+    used_shorthands = set()
+    for name, func in inspect.getmembers(Jenkins, predicate=inspect.isfunction):
+        if not hasattr(func, "is_visible"):
+            continue
+
+        shorthand = name[0]
+        if shorthand in used_shorthands:
+            shorthand = name[:2]
+
+        used_shorthands.add(shorthand)
+        sub = subparsers.add_parser(name, aliases=[shorthand], help=func.__doc__)
+        sig = inspect.signature(func)
+        for param_name, param in sig.parameters.items():
+            if param_name == "self":
+                continue
+            kwargs = {}
+            kwargs["required"] = True
+            sub.add_argument(f"--{param_name.replace('_', '-')}", **kwargs)
+        sub.set_defaults(func=func)
+    return parser
+
+
+def main() -> None:
+    jk = Jenkins()
+    parser = create_cli(Jenkins)
+    args = parser.parse_args()
+
+    func_args = vars(args).copy()
+
+    command_name = func_args.pop("func").__name__
+    func_args.pop("command")
+    func_args.pop("func", None)
+
+    method_to_call = getattr(jk, command_name)
+
+    method_to_call(**func_args)
+
+
+if __name__ == "__main__":
+    main()
